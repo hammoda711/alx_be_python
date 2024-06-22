1c4d799163a0bfa71cb923bd36069ea140cd7764
Mohamed Taher <66457688+hammoda711@users.noreply.github.com>
2024-06-23 01:07:06 +0300

    Update basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,6 @@
+number1 = 10
+number2 = 5
+additional= number1 + number2
+subtraction=number1 - number2
+multiplication= number1 * number2
+print(f' Addition of 10 and 5 is  {additional}\n',f'Subtraction of 10 and 5 is {subtraction}\n',f'Multiplication of 10 and 5 is{multiplication}')
