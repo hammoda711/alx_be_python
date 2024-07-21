e6acfeabca6fcb36cc7bc3b5a817d3f33fca1826
Mohamed Taher <mo.taher717@gmail.com>
2024-07-22 01:39:37 +0300

    unittesting-2

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -8,7 +8,7 @@ class TestSimpleCalculator(unittest.TestCase):
         """Set up the calculator instance for use in the tests."""
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         """Test the add method."""
         self.assertEqual(self.calc.add(1, 2), 3)
         self.assertEqual(self.calc.add(-1, -1), -2)
