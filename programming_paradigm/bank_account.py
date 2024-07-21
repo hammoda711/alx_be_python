508df24954352149b3a3a253ed3c553cd1623490
Mohamed Taher <mo.taher717@gmail.com>
2024-07-22 00:23:21 +0300

    bank account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,26 @@
+class BankAccount:
+    def __init__(self,initial_balance=0):
+        self.account_balance = initial_balance
+    
+    
+    def deposit(self,amount):
+        self.account_balance += amount
+
+
+
+    def withdraw(self,amount):
+        if self.account_balance >= amount:
+            self.account_balance -= amount
+            return True
+        else: return False
+
+
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance}")
+
+    if __name__ == "__main__":
+        # Example usage
+        account = BankAccount(100)
+        account.deposit(50)
+        account.withdraw(30)
+        account.display_balance()
