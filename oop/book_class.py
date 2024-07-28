ebf42d5db37fcb72fb8a06a31b2281839f26dd5b
Mohamed Taher <mo.taher717@gmail.com>
2024-07-28 19:57:42 +0300

    second commit book class with magic methods

--- oop/book_class.py
+++ oop/book_class.py
@@ -2,14 +2,14 @@ class Book:
     def __init__(self,title,author, year):
 
         self.title = title
-        self.auther = author
+        self.author = author
         self.year = year
 
     def __del__(self):
         print(f"Deleting {self.title}") 
     
     
-    def __str__(self) -> str:
+    def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
     
     
